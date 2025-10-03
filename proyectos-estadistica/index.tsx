5b8af2615242b51f8e863499c0660d71df25aef1
cimejia <87611946+cimejia@users.noreply.github.com>
2025-10-03 13:51:22 -0500

    Add files via upload

---
+++ proyectos-estadistica/index.tsx
@@ -0,0 +1,8 @@
+/**
+ * @license
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+// This file is intentionally left blank as the current functionality
+// is achieved with HTML and CSS alone.
+// Future interactive features can be added here.
